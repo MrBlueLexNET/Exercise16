b3f593bb396975d304c01697a3a3e02368f9d2ed
JYM <jeanyvesmichel@yahoo.com>
2023-02-15 09:41:22 +0100

    Device + Location Entities

---
+++ Exercise16/Shared/Entities/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16.Shared.Entities
+{
+    public class Device
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public string Name { get; set; } = string.Empty;
+        public bool Online { get; set; }
+        public int LocationId { get; set; }
+        public Location? Location { get; set; }
+
+        
+    }
+}
---
+++ Exercise16/Shared/Entities/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16.Shared.Entities
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string CityTown { get; set; } = string.Empty;
+        public string StateProvince { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
