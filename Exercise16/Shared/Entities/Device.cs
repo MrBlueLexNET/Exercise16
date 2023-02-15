a14c4dcdcbc10b82394c2ca539e454910a5202d9
JYM <jeanyvesmichel@yahoo.com>
2023-02-15 15:22:36 +0100

    DevicesController Added

--- Exercise16/Shared/Entities/Device.cs
+++ Exercise16/Shared/Entities/Device.cs
@@ -8,7 +8,7 @@ namespace Exercise16.Shared.Entities
 {
     public class Device
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public string DeviceId { get; set; } = Guid.NewGuid().ToString("n");
         public string Name { get; set; } = string.Empty;
         public bool Online { get; set; }
         public int LocationId { get; set; }
